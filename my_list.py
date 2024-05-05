8e1a59a511515a2bc7f61eab4c6a1173403c9b80
Roslaan001 <abdulsomad005@gmail.com>
2024-05-05 11:48:48 +0100

    first commit

---
+++ my_list.py
@@ -0,0 +1,29 @@
+my_list = []
+
+my_list.append(10)
+my_list.append(20)
+my_list.append(30)
+my_list.append(40)
+
+print (my_list)
+
+my_list[2] = 15
+
+print (my_list)
+
+my_list1 = [50, 60, 70]
+
+my_list.extend (my_list1)
+
+print (my_list)
+
+
+my_list.pop()
+
+print (my_list)
+
+my_list.sort()
+
+print (my_list[4])
+
+print (my_list.index(40))
